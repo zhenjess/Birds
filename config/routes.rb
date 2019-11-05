30debe9096e7a2f93d0c1a5d34e8b8cfcc836e0f
zhenjess <jessicazhen8@gmail.com>
2019-11-04 17:41:16 -0800

    Create Birds FSP

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: :json} do 
+    resources :shoes, only: [:index, :show, :create, :update, :edit]
+    resources :accessories, only: [:index, :show, :create, :update, :edit]
+    resources :materials, only: [:index, :show, :create]
+    resources :reviews, only: [:create, :show, :edit, :destroy]
+    resource :user, only: [:create]
+    resource :cart 
+  end
+  root "static_pages#root"
+end
