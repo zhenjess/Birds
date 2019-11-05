c10c5d477674d85ec8aa4b224ea8b4ae95bafed1
zhenjess <jessicazhen8@gmail.com>
2019-11-05 15:21:18 -0800

    Add Session Form Container

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -4,6 +4,8 @@ import { Provider } from "react-redux";
 
 import { HashRouter } from "react-router-dom";
 
+import App from './app';
+
 const Root = ({ store }) => (
     <Provider store={ store }>
         <HashRouter>
