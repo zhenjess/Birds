9f853b622fa1e15b33daf8676b99bc4a39d17ea4
zhenjess <jessicazhen8@gmail.com>
2019-11-05 11:01:21 -0800

    Update sessions controller to check email

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,10 +1,10 @@
 class Api::SessionsController < ApplicationController 
     def create
-        @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+        @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
 
         if @user
             login(@user)
-            render "api/users/show"
+            render :show
         else
             render json: ["Invalid username/password combination"], status: 401
         end
@@ -16,7 +16,7 @@ class Api::SessionsController < ApplicationController
             logout
             render "api/users/show"
         else 
-            render json: ["Nobody signed in"], status: 404
+            render json: ["No current user"], status: 404
         end
     end
 end
