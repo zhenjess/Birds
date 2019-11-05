3bafea3ab1511bef8b2da555b338561a3d7aa5e3
zhenjess <jessicazhen8@gmail.com>
2019-11-05 14:29:15 -0800

    Update entry file

--- frontend/birds.jsx
+++ frontend/birds.jsx
@@ -1,13 +1,27 @@
+//React
 import React from "react";
 import ReactDOM from "react-dom";
-import * as APIUtil from './util/session_api_util';
-// import Root from './components/root';
-// import configureStore from './store/store';
+// import * as APIUtil from './util/session_api_util';
+//Components
+import Root from './components/root';
+import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
-    // window.login = APIUtil.login;
-    // window.signup = APIUtil.signup;
-    // window.logout = APIUtil.logout;
+    let store;
+    if (window.currentUser) {
+        const preloadedState = {
+            session: { id: window.currentUser.id },
+            entities: { 
+                users: { [window.currentUser.id]: window.currentUser }
+            }
+        };
+        store = configureStore(preloadedState);
+        delete window.currentUser;
+    } else {
+        store = configureStore();
+    }
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
     const root = document.getElementById('root');
-    ReactDOM.render(<h1>Welcome to Birds</h1>, root); 
+    ReactDOM.render(<Root store={store}/>, root); 
 });
