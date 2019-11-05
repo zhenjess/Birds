c0d2ee8dbb3e8d200493ce63cd0850239c49792f
zhenjess <jessicazhen8@gmail.com>
2019-11-05 13:40:23 -0800

    Add session reducer

--- frontend/birds.jsx
+++ frontend/birds.jsx
@@ -5,9 +5,9 @@ import * as APIUtil from './util/session_api_util';
 // import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
-    window.login = APIUtil.login;
-    window.signup = APIUtil.signup;
-    window.logout = APIUtil.logout;
+    // window.login = APIUtil.login;
+    // window.signup = APIUtil.signup;
+    // window.logout = APIUtil.logout;
     const root = document.getElementById('root');
     ReactDOM.render(<h1>Welcome to Birds</h1>, root); 
 });
--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,8 +1,25 @@
 import { 
     RECEIVE_CURRENT_USER, 
-    LOGOUT_CURRENT_USER,
+    LOGOUT_CURRENT_USER 
 } from '../actions/session_actions';
 
-const _nullUser = Object.freeze({
-   id: null
-})
+const _nullUser = Object.freeze({ id: null });
+
+const sessionReducer = (state = _nullUser, action) => {
+    Object.freeze(state);
+    let newState;
+
+    switch(action.type) {
+        case RECEIVE_CURRENT_USER:
+            newState = Object.assign({}, state, {id: action.currentUser.id});
+            return newState;
+
+        case LOGOUT_CURRENT_USER:
+            newState = _nullUser;
+            return newState;
+        default:
+            return state;
+    }
+};
+
+export default sessionReducer;
