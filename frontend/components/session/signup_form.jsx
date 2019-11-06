f366bca5453c94d7cefc2ce1fbf70b300510ff28
zhenjess <jessicazhen8@gmail.com>
2019-11-05 18:19:32 -0800

    Create and update signup form

--- frontend/components/session/signup_form.jsx
+++ frontend/components/session/signup_form.jsx
@@ -42,7 +42,57 @@ class SignupForm extends React.Component {
             );
         }
 
-        
+        render() {
+            return (
+                <div>
+                    <form onSubmit={this.handleSubmit}>
+                        <h1>CREATE AN ACCOUNT</h1>
+
+                        <ul>
+                            <li>Your credit card information will not be saved.</li>
+                            <li>Register for fast and easy checkout and for save account order information.</li>
+                        </ul>
+
+                        {this.renderErrors()}
+                        <div classname="signup-form">
+                            <br/>
+                            <label>FIRST NAME
+                                <input 
+                                    type="text"
+                                    value={this.state.first_name}
+                                    onChange={this.update('first_name')}
+                                />
+                            </label>
+                            <label>LAST NAME
+                                <input
+                                    type="text"
+                                    value={this.state.last_name}
+                                    onChange={this.update('last_name')}
+                                />
+                            </label>
+                            <label>EMAIL*
+                                <input
+                                    type="text"
+                                    value={this.state.email}
+                                    onChange={this.update('email')}
+                                />
+                            </label>
+                            <label>PASSWORD*
+                                <input
+                                    type="password"
+                                    value={this.state.password}
+                                    onChange={this.update('password')}
+                                />
+                            </label>
+                            <br/>
+                            <input type="submit" className="submit" value="REGISTER"/>
+                        </div>
+                    </form>
+                    <p>By creating an account, you agree to our Terms of Use and Privacy Policy.</p>
+                    <p id="required-fields">* REQUIRED FIELDS</p>
+                </div>
+            );
+        }
     }
 }
 
