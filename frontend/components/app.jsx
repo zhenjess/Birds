b5b8b9e0b106967fdf8df6c8423096911fcb6c9e
zhenjess <jessicazhen8@gmail.com>
2019-11-05 19:06:21 -0800

    Update App file

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 
+import { Route, Redirect, Switch, Link, HashRouter } from 'react-router-dom';
+
+import SessionFormContainer from './session/session_form_container';
+
+import NavBarContainer from './navbar/navbar_container';
+
+import Main from './main/main_container';
+
 const App = () => (
     <div>
-        <h1>Birds</h1>
+        <Header />
+        <Switch>
+            <Route exact path="/session" component={SessionFormContainer}/>
+        </Switch>/>
     </div>
 );
 
