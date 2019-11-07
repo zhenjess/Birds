251f77deff4b129655b212c14eac2334d9e6d6f9
zhenjess <jessicazhen8@gmail.com>
2019-11-06 18:57:14 -0800

    Update navbar components html and css

--- frontend/components/navbar/navbar_form.jsx
+++ frontend/components/navbar/navbar_form.jsx
@@ -2,38 +2,24 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
-// export default ({ currentUser, logout }) => {
-//     const display = currentUser ? (
-//         <div>
-
-//         </div>
-//     ) : (
-//         <div>
-//             <Link className="button" to="/MEN">MEN</Link>
-//             <Link className="button" to="/WOMEN">WOMEN</Link>
-//             <Link className="button" to="/KIDS">KIDS</Link>
-//             <Link className="button" to="MATERIALS">MATERIALS</Link>
-//             <Link className="button" to="STORES">STORES</Link>
-//             <Link className="button" to="ACCOUNT">ACCOUNT</Link>
-//             <Link className="button" to="?">?</Link>
-//             <Link className="button" to="CART">Cart</Link>
-//         </div>
-//     );
-
-//     return (
-//         <header className="navbar">
-//             <h1 className="logo">birds</h1>
-//             <div>
-//                 {display}
-//             </div>
-//         </header>
-//     );
-// }
 
 class NavbarForm extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
-        return(
-            <div className="navbar">
+        return (
+            <div className="nav-left">
+                <div className="dropdown">
+                    <button className="dropbtn">FEMALE
+                        <i className="fa fa-caret-down"></i>
+                    </button>
+                    <div class="dropdown-content">
+                        <a href="#">Shop ALL BIRDS</a>
+                    </div>
+
+                </div>
                 <div className="dropdown">
                     <button className="dropbtn">MALE
                         <i className="fa fa-caret-down"></i>
@@ -42,29 +28,22 @@ class NavbarForm extends React.Component {
                         <a href="#">Shop ALL BIRDS</a>
                     </div>
                 </div>
-
-                <div className="dropdown">
-                    <button class="dropbtn">FEMALE
-                        <i class="fa fa-caret-down"></i>
-                    </button>
-                    <div class="dropdown-content">
-                        <a href="#">Shop ALL BIRDS</a>
-                    </div>
-                </div>
-
                 <a href="#chicks">CHICKS</a>
-                
-                <header className="navbar">
-                    <h1 className="logo">birds</h1>
-                </header>
+            </div>
 
-                <div className="links">
-                    <a href="#materials">MATERIALS</a>
-                    <a href="#stores">STORES</a>
-                    <a href="#account">ACCOUNT</a>
-                    <a href="?">?</a>
-                    <a href="nest">NEST</a>
-                </div>
+            <div className="logo">
+                <style>
+                    @import url('https://fonts.googleapis.com/css?family=Lobster&display=swap');
+                </style>
+                <h1>birds</h1>
+            </div>
+
+            <div className="nav-right">
+                <a href="#materials">MATERIALS</a>
+                <a href="#stores">STORES</a>
+                <a href="#account">ACCOUNT</a>
+                <a href="?">?</a>
+                <a href="nest">NEST</a>
             </div>
         );
     }
