a604f9a093dbae9898f99f7e4034508ca6dec4ea
zhenjess <jessicazhen8@gmail.com>
2019-11-06 16:56:37 -0800

    Update main

--- frontend/components/main/main.jsx
+++ frontend/components/main/main.jsx
@@ -11,8 +11,8 @@ class Main extends React.Component {
                 <div className="hero-img-frame">
                     {/* <img className="hero-img" src="https://www.instagram.com/p/B2qyIVCpTyj/?utm_source=ig_web_button_share_sheet" alt=""/> */}
                     <h1>BETTER SHOES IN A BETTER WAY</h1>
-                    <button>Shop Men</button>
-                    <button>Shop Women</button>
+                    <button>Shop FEMALE</button>
+                    <button>Shop MALE</button>
                 </div>
             </div>
         );
