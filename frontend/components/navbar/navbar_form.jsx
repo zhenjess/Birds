3aa3cc6b2bf981ab64037388da4d800bae12c45a
zhenjess <jessicazhen8@gmail.com>
2019-11-06 19:23:31 -0800

    Update navbar css and html

--- frontend/components/navbar/navbar_form.jsx
+++ frontend/components/navbar/navbar_form.jsx
@@ -10,40 +10,42 @@ class NavbarForm extends React.Component {
 
     render() {
         return (
-            <div className="nav-left">
-                <div className="dropdown">
-                    <button className="dropbtn">FEMALE
+            <div>
+                <div className="nav-left">
+                    <div className="dropdown">
+                        <button className="dropbtn">FEMALE
                         <i className="fa fa-caret-down"></i>
-                    </button>
-                    <div class="dropdown-content">
-                        <a href="#">Shop ALL BIRDS</a>
-                    </div>
+                        </button>
+                        <div className="dropdown-content">
+                            <a href="#">Shop ALL BIRDS</a>
+                        </div>
 
-                </div>
-                <div className="dropdown">
-                    <button className="dropbtn">MALE
+                    </div>
+                    <div className="dropdown">
+                        <button className="dropbtn">MALE
                         <i className="fa fa-caret-down"></i>
-                    </button>
-                    <div class="dropdown-content">
-                        <a href="#">Shop ALL BIRDS</a>
+                        </button>
+                        <div className="dropdown-content">
+                            <a href="#">Shop ALL BIRDS</a>
+                        </div>
                     </div>
+                    <a href="#kids">KIDS</a>
                 </div>
-                <a href="#chicks">CHICKS</a>
-            </div>
 
-            <div className="logo">
-                <style>
-                    @import url('https://fonts.googleapis.com/css?family=Lobster&display=swap');
+                <div className="logo">
+                    <style>
+                        @import url('https://fonts.googleapis.com/css?family=Lobster&display=swap');
                 </style>
-                <h1>birds</h1>
-            </div>
+                    <h1>birds</h1>
+                </div>
 
-            <div className="nav-right">
-                <a href="#materials">MATERIALS</a>
-                <a href="#stores">STORES</a>
-                <a href="#account">ACCOUNT</a>
-                <a href="?">?</a>
-                <a href="nest">NEST</a>
+                <div className="nav-right">
+                    <a href="#materials">MATERIALS</a>
+                    <a href="#stores">STORES</a>
+                    <a href="#account">ACCOUNT</a>
+                    <a href="?">?</a>
+                    <a href="cart">CART</a>
+                </div>
             </div>
         );
     }
