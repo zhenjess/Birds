ebf3f7fc648132b117f05609e1278a6d4aba7b9f
zhenjess <jessicazhen8@gmail.com>
2019-11-06 17:51:39 -0800

    Update main component css

--- frontend/components/main/main.jsx
+++ frontend/components/main/main.jsx
@@ -8,12 +8,14 @@ class Main extends React.Component {
     render() {
         return (
             <div class="main-image">
-                <div class="main-text">
+                <div class="header-text">
                     <style>
                         @import url('https://fonts.googleapis.com/css?family=Merriweather+Sans:800i&display=swap');
                     </style>
                     <h1>BETTER SHOES</h1>
                     <h1>IN A BETTER WAY</h1>
+                </div>
+                <div class="button-text">
                     <button>SHOP FEMALE</button>
                     <button>SHOP MALE</button>
                 </div>
