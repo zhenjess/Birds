73fa5f9ab3e4289686ce13404909ba160d8bc827
zhenjess <jessicazhen8@gmail.com>
2019-11-08 12:09:36 -0800

    Edit materials component on main

--- frontend/components/main/materials_main.jsx
+++ frontend/components/main/materials_main.jsx
@@ -13,11 +13,32 @@ class MaterialsMain extends React.Component {
                 <div className="images-panel">
                     <div className='left-image-panel'>
                         <h2>WOOL</h2>
-                        <p></p>
+                        <p>Warm &amp; Cozy</p>
+
+                        <div className="left-links">
+                            <div className="left-link-text">
+                                <Link className="left-link" to="/collections/female">SHOP FEMALE</Link>
+                            </div>
+
+                            <div className="left-link-text">
+                                <Link className="left-link" to="/collections/male">SHOP MALE</Link>
+                            </div>
+                        </div>
                     </div>
 
                     <div className='right-image-panel'>
+                        <h2>TREE</h2>
+                        <p>Light &amp; Breezy</p>
 
+                        <div className="left-links">
+                            <div className="left-link-text">
+                                <Link className="left-link" to="/collections/female">SHOP FEMALE</Link>
+                            </div>
+
+                            <div className="left-link-text">
+                                <Link className="left-link" to="/collections/male">SHOP MALE</Link>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
