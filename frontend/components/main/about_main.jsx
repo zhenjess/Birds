0c005d5c6a1b445c8022be083a3521c7a2a7678b
zhenjess <jessicazhen8@gmail.com>
2019-11-08 15:56:21 -0800

    Update main css

--- frontend/components/main/about_main.jsx
+++ frontend/components/main/about_main.jsx
@@ -19,10 +19,10 @@ class AboutMain extends React.Component {
                     <style>
                         @import url('https://fonts.googleapis.com/css?family=Merriweather+Sans&display=swap');
                     </style>
-                    <p>It all starts from the finest materials Mother Nature offers.</p>
-                    <p>Designed practically and made from sustainable resources.</p>
-                    <p>Closing the loop with ZEROING waste because we care about OUR planet.</p>
-                    <p>That sounds better, right?</p>
+                    <p>It all starts from the finest materials Mother Nature offers.
+                    Designed practically and made from sustainable resources.
+                    Closing the loop with ZEROING waste because we care about OUR planet.
+                    That sounds better, right?</p>
                     <Link className="about-main-link" to={"/collections/male"}>
                         <input 
                             type="submit"
--- frontend/components/main/materials_main.jsx
+++ frontend/components/main/materials_main.jsx
@@ -11,7 +11,25 @@ class MaterialsMain extends React.Component {
         return (
             <div className='materials-main'>
                 <div className="images-panel">
+
                     <div className='left-image-panel'>
+                        <div className="left-image">
+                            <div className="left-image-text">
+                                
+                                <h2>Wool</h2>
+                                <p>Warm &amp; Cozy</p>
+                                <div className="left-links">
+                                    <div className="left-link-text">
+                                        <Link className="left-link" to="/collections/female">SHOP FEMALE</Link>
+                                    </div>
+
+                                    <div className="left-link-text">
+                                        <Link className="left-link" to="/collections/male">SHOP MALE</Link>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
                         <div className="left-gif"></div>
                         <div className='left-small-image'></div>
 
@@ -19,18 +37,6 @@ class MaterialsMain extends React.Component {
                             @import url('https://fonts.googleapis.com/css?family=Roboto:700&display=swap');
                         </style>
 
-                        <h2>WOOL</h2>
-                        <p>Warm &amp; Cozy</p>
-
-                        <div className="left-links">
-                            <div className="left-link-text">
-                                <Link className="left-link" to="/collections/female">SHOP FEMALE</Link>
-                            </div>
-
-                            <div className="left-link-text">
-                                <Link className="left-link" to="/collections/male">SHOP MALE</Link>
-                            </div>
-                        </div>
                     </div>
 
                     <div className='right-image-panel'>
