426fd6cfa4361c0d2ff3bffa950fe0eeed8c4d71
zhenjess <jessicazhen8@gmail.com>
2019-11-08 10:40:01 -0800

    Add about main component

--- frontend/components/main/about_main.jsx
+++ frontend/components/main/about_main.jsx
@@ -10,17 +10,26 @@ class AboutMain extends React.Component {
     render() {
         return (
             <div className="about-main">
-                <h2>Natural Comfort Sourced from Natural Materials</h2>
-                <p>It all starts from the finest materials Mother Nature offers.</p>
-                <p>Designed practically and made from sustainable resources.</p>
-                <p>Closing the loop with ZEROING waste because we care about OUR planet.</p>
-                <p>That sounds better, right?</p>
-                <Link className="about-main-link" to={"/collections/male"}>
-                    <input 
-                        type="submit"
-                        value="LEARN MORE ABOUT OUR MATERIALS"
-                    />
-                </Link>
+                <div className="about-image"></div>
+                <div className="about-text">
+                    <style>
+                        @import url('https://fonts.googleapis.com/css?family=Merriweather+Sans:700&display=swap');
+                    </style>
+                    <h2>Natural Comfort Sourced from Natural Materials</h2>
+                    <style>
+                        @import url('https://fonts.googleapis.com/css?family=Merriweather+Sans&display=swap');
+                    </style>
+                    <p>It all starts from the finest materials Mother Nature offers.</p>
+                    <p>Designed practically and made from sustainable resources.</p>
+                    <p>Closing the loop with ZEROING waste because we care about OUR planet.</p>
+                    <p>That sounds better, right?</p>
+                    <Link className="about-main-link" to={"/collections/male"}>
+                        <input 
+                            type="submit"
+                            value="LEARN MORE ABOUT OUR MATERIALS"
+                        />
+                    </Link>
+                </div>
             </div>
         );
     }
