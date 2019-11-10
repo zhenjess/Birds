87619a2598741ff7d83cc3f4660ae5f2fbf17389
zhenjess <jessicazhen8@gmail.com>
2019-11-09 18:16:40 -0800

    Update shoe models

--- app/models/color.rb
+++ app/models/color.rb
@@ -2,5 +2,7 @@ class Color < ApplicationRecord
     validates :hue, presence: true
 
     has_many :shoe_colors
-    has_many :shoes, through: :shoe_colors
+    has_many :shoes, 
+        through: :shoe_colors
+        source: :shoe
 end
--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -2,8 +2,12 @@ class Shoe < ApplicationRecord
     validates :model, :gender, presence: true
 
     has_many :shoe_materials
-    has_many :materials, through: :shoe_materials
+    has_many :materials, 
+        through: :shoe_materials, 
+        source: :material
 
     has_many :shoe_colors
-    has_many :colors, through: :shoe_colors
+    has_many :colors, 
+        through: :shoe_colors, 
+        source: :color
 end
