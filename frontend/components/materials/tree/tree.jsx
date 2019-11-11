6e5db2e6268a9f3c702e8f1fbd22ed431be9ba1e
zhenjess <jessicazhen8@gmail.com>
2019-11-11 00:46:41 -0800

    Update materials links

--- frontend/components/materials/tree/tree.jsx
+++ frontend/components/materials/tree/tree.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
+import Materials from '../materials';
+
 class Tree extends React.Component {
     constructor(props) {
         super(props);
