b2e2573be061fb568e60c4b384fd3894b4d62282
zhenjess <jessicazhen8@gmail.com>
2019-11-11 18:08:50 -0800

    Implement shoe and shoe material reducers

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -4,9 +4,16 @@ import usersReducer from './users_reducer';
 
 import shoesReducer from './shoes_reducer';
 
+import materialsReducer from './materials_reducer';
+
+import shoeMaterialsReducer from './shoe_materials_reducer';
+
 const entitiesReducer = combineReducers({
     users: usersReducer, 
-    shoes: shoesReducer
+    shoes: shoesReducer, 
+    materials: materialsReducer, 
+    shoeMaterials: shoeMaterialsReducer
 });
 
-export default entitiesReducer;
+export default entitiesReducer; //represents data from db and manages it
+//then delegates tasks to each reducer
---
+++ frontend/reducers/shoe_materials_reducer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { RECEIVE_ALL_SHOES } from '../actions/shoes_actions'; //import action type
+
+const shoeMaterialsReducer = (state = {}, action) => {
+    Object.freeze(state);
+
+    switch (action.type) {
+        case RECEIVE_ALL_SHOES:
+            return action.payload.shoeMaterials;
+        default:
+            return state;
+    }
+};
+
+export default shoeMaterialsReducer;
