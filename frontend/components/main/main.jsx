6bc2bef8e7200adaa574af56bb15d2692e40787e
zhenjess <jessicazhen8@gmail.com>
2019-11-14 13:43:53 -0800

    Update main show page css

--- frontend/components/main/main.jsx
+++ frontend/components/main/main.jsx
@@ -13,11 +13,9 @@ class Main extends React.Component {
         return (
             <div className="main-image">
                 <div className="header-text">
-                    <style>
-                        @import url('https://fonts.googleapis.com/css?family=Merriweather+Sans:800i&display=swap');
-                    </style>
-                    <h1>BETTER SHOES</h1>
-                    <h1>IN A BETTER WAY</h1>
+                  
+                    <p>BETTER SHOES</p>
+                    <p>IN A BETTER WAY</p>
                 </div>
 
                 <div className="main-links">
