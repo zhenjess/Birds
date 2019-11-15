d235752b0a258f828c00ec0ca2b6d8b83f8bae80
zhenjess <jessicazhen8@gmail.com>
2019-11-15 10:02:39 -0800

    Update shoe bar

--- frontend/components/shoes/shoes_header.jsx
+++ frontend/components/shoes/shoes_header.jsx
@@ -93,7 +93,13 @@ const ShoesHeader = ({ gender }) => {
                         <span className="bar-link">Size</span>
                         <i class="fas fa-caret-down"></i>
                         <div className="dropdown-content">
-                            <p>sizes</p>
+                            <p>5</p>
+                            <p>6</p>
+                            <p>7</p>
+                            <p>8</p>
+                            <p>9</p>
+                            <p>10</p>
+                            <p>11</p>
                         </div>
                     </div>
 
@@ -101,7 +107,12 @@ const ShoesHeader = ({ gender }) => {
                         <span className="bar-link">Hue</span>
                         <i class="fas fa-caret-down"></i>
                         <div className="dropdown-content">
-                            <p>colors</p>
+                            <p>Black</p>
+                            <p>Grey</p>
+                            <p>White</p>
+                            <p>Red</p>
+                            <p>Green</p>
+                            <p>Blue</p>
                         </div>
                     </div>
 
@@ -109,7 +120,10 @@ const ShoesHeader = ({ gender }) => {
                         <span className="bar-link">Model</span>
                         <i class="fas fa-caret-down"></i>
                         <div className="dropdown-content">
-                            <p>models</p>
+                            <p>Runners</p>
+                            <p>Loungers</p>
+                            <p>Skippers</p>
+                            <p>Toppers</p>
                         </div>
                     </div>
 
@@ -117,7 +131,8 @@ const ShoesHeader = ({ gender }) => {
                         <span className="bar-link">Material</span>
                         <i class="fas fa-caret-down"></i>
                         <div className="dropdown-content">
-                            <p>materials</p>
+                            <p>Wool</p>
+                            <p>Tree</p>
                         </div>
                     </div>
                 </div>
