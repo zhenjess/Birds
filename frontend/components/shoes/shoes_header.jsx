8e232f86e15064b8e260f42ecd51320f837ab2ad
zhenjess <jessicazhen8@gmail.com>
2019-11-15 07:34:41 -0800

    Update shoe index show page

--- frontend/components/shoes/shoes_header.jsx
+++ frontend/components/shoes/shoes_header.jsx
@@ -59,12 +59,55 @@ const ShoesHeader = ({ gender }) => {
     } 
 
     return (
-        <div className="male-shoe-image">
-            <div className="shoe-header-text">
-                <p className="shoe-header-title">Male Shoes</p>
-                <p className="shoe-header-texts">The world's most comfortable shoes for life’s everyday adventures.</p>
-            </div>
-        </div>   
+        <div>
+
+            <div className="male-shoe-image">
+                <div className="shoe-header-text">
+                    <p className="shoe-header-title">Male Shoes</p>
+                    <p className="shoe-header-texts">The world's most comfortable shoes for life’s everyday adventures.</p>
+                </div>
+            </div>  
+
+            <div className="shoe-bar">
+                <div className="left-bar">
+                    <p>All- 33 Results</p>
+                </div>
+
+                <div className="right-bar">
+                    <div className="dropdown">
+                        <span className="bar-link">Size</span>
+                        <i class="fas fa-caret-down"></i>
+                        <div className="dropdown-content">
+                            <p>sizes</p>
+                        </div>
+                    </div>
+
+                    <div className="dropdown">
+                        <span className="bar-link">Hue</span>
+                        <i class="fas fa-caret-down"></i>
+                        <div className="dropdown-content">
+                            <p>colors</p>
+                        </div>
+                    </div>
+
+                    <div className="dropdown">
+                        <span className="bar-link">Model</span>
+                        <i class="fas fa-caret-down"></i>
+                        <div className="dropdown-content">
+                            <p>models</p>
+                        </div>
+                    </div>
+
+                    <div className="dropdown">
+                        <span className="bar-link">Material</span>
+                        <i class="fas fa-caret-down"></i>
+                        <div className="dropdown-content">
+                            <p>materials</p>
+                        </div>
+                    </div>
+                </div>
+            </div> 
+        </div>
     );
 };
 
