a9204d9a11d597e3a27e0807ce673ee3fed4fab3
zhenjess <jessicazhen8@gmail.com>
2019-11-15 10:00:43 -0800

    Add content to shoe bar

--- frontend/components/shoes/shoes_header.jsx
+++ frontend/components/shoes/shoes_header.jsx
@@ -25,7 +25,13 @@ const ShoesHeader = ({ gender }) => {
                             <span className="bar-link">Size</span>
                             <i class="fas fa-caret-down"></i>
                             <div className="dropdown-content">
-                                <p>sizes</p>
+                                <p>5</p>
+                                <p>6</p>
+                                <p>7</p>
+                                <p>8</p>
+                                <p>9</p>
+                                <p>10</p>
+                                <p>11</p>
                             </div>
                         </div>
 
@@ -33,7 +39,12 @@ const ShoesHeader = ({ gender }) => {
                             <span className="bar-link">Hue</span>
                             <i class="fas fa-caret-down"></i>
                             <div className="dropdown-content">
-                                <p>colors</p>
+                                <p>Black</p>
+                                <p>Grey</p>
+                                <p>White</p>
+                                <p>Red</p>
+                                <p>Green</p>
+                                <p>Blue</p>
                             </div>
                         </div>
 
@@ -41,7 +52,10 @@ const ShoesHeader = ({ gender }) => {
                             <span className="bar-link">Model</span>
                             <i class="fas fa-caret-down"></i>
                             <div className="dropdown-content">
-                                <p>models</p>
+                                <p>Runners</p>
+                                <p>Loungers</p>
+                                <p>Skippers</p>
+                                <p>Toppers</p>
                             </div>
                         </div>
 
@@ -49,7 +63,8 @@ const ShoesHeader = ({ gender }) => {
                             <span className="bar-link">Material</span>
                             <i class="fas fa-caret-down"></i>
                             <div className="dropdown-content">
-                                <p>materials</p>
+                                <p>Wool</p>
+                                <p>Tree</p>
                             </div>
                         </div>
                     </div>
