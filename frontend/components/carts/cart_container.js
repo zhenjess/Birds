a93cff0bc04b1e8c1b3eb13369b31573d6f62cf2
zhenjess <jessicazhen8@gmail.com>
2019-12-19 13:30:22 -0800

    Create cart container

---
+++ frontend/components/carts/cart_container.js
@@ -0,0 +1,17 @@
+import Cart from './cart';
+import { connect } from 'react-redux';
+import { removeFromCart } from '../../actions/cart_actions';
+
+const mapStateToProps = state => {
+    return ({
+        items: Object.values(state.entities.cart)
+    });
+};
+
+const mapDispatchToProps = dispatch => {
+    return ({
+        removeFromCart: (id, size) => dispatch(removeFromCart(id, size))
+    });
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
