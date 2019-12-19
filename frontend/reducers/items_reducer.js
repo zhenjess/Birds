c5df7ea74614b3aabe0d90fd12d757c76907826d
zhenjess <jessicazhen8@gmail.com>
2019-12-19 15:58:05 -0800

    Implement items reducer

---
+++ frontend/reducers/items_reducer.js
@@ -0,0 +1,16 @@
+import { RECEIVE_ITEMS } from '../actions/items_actions';
+
+const itemsReducer = (oldState={}, action) => {
+    Object.freeze(oldState);
+
+    let newState;
+    switch(action.type) {
+        case RECEIVE_ITEMS:
+            newState = Object.assign({}, oldState, action.items);
+            return newState;
+        default:
+            return oldState;
+    }
+};
+
+export default itemsReducer;
