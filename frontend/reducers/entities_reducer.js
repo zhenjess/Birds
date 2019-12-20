56bb8ccc7432db66a90b7ac2b97d24893eff5732
zhenjess <jessicazhen8@gmail.com>
2019-12-19 16:22:44 -0800

    Update entities reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -10,12 +10,18 @@ import colorsReducer from './colors_reducer';
 
 import shoeOptionsReducer from './shoe_options_reducer';
 
+import itemsReducer from './items_reducer';
+
+import cartsReducer from './carts_reducer';
+
 const entitiesReducer = combineReducers({
     users: usersReducer, 
     shoes: shoesReducer, 
     materials: materialsReducer, 
     colors: colorsReducer,
-    shoeOptions: shoeOptionsReducer
+    shoeOptions: shoeOptionsReducer,
+    items: itemsReducer, 
+    carts: cartsReducer
 });
 
 export default entitiesReducer; //represents data from db and manages it
