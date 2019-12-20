486920e14908fe25b9f8fa0538ead997dd53bdc0
zhenjess <jessicazhen8@gmail.com>
2019-12-20 10:25:06 -0800

    Migrate cart tables

---
+++ db/migrate/20191220175733_create_cart_items.rb
@@ -0,0 +1,11 @@
+class CreateCartItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cart_items do |t|
+      t.integer :cart_id, null: false
+      t.integer :item_id, null: false
+      t.integer :quantity, null: false
+    end
+    add_index :cart_id, unique: true
+    add_index :item_id, unique: true
+  end
+end
