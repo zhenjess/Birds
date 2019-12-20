3413023a81c1444b1a3f1e343242c920f908e984
zhenjess <jessicazhen8@gmail.com>
2019-12-19 16:13:30 -0800

    Create cart reducer

---
+++ frontend/reducers/carts_reducer.js
@@ -0,0 +1,23 @@
+import { 
+    POST_TO_CART, 
+    REMOVE_FROM_CART 
+} from '../actions/cart_actions';
+
+const cartReducer = (oldState={}, action) => {
+    Object.freeze(oldState);
+
+    let newState;
+    switch(action.type) {
+        case POST_TO_CART:
+            newState = Object.assign({}, oldState, action.item);
+            return newState;
+        case REMOVE_FROM_CART:
+            newState = Object.assign({}, oldState);
+            delete newState[`${action.id}-${action.size}`];
+            return newState;
+        default: 
+            return oldState;
+    }
+};
+
+export default cartReducer;
