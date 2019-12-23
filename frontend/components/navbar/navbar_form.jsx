1c67ba6ab3f59447a06802686a13d7e181869e67
zhenjess <jessicazhen8@gmail.com>
2019-12-22 18:46:19 -0800

    Readjust navbar

--- frontend/components/navbar/navbar_form.jsx
+++ frontend/components/navbar/navbar_form.jsx
@@ -1,189 +1,21 @@
-// import React from 'react';
-
-// import { Link } from 'react-router-dom';
-
-
-// class NavbarForm extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         // this.handleNestClick = this.handleNestClick.bind(this);
-//         this.handleSubmit = this.handleSubmit.bind(this)
-//         this.state = { processed: false }; 
-//     }
-
-//     // handleNestClick() {
-//     //     e.preventDefault();
-//     //     const nest = this.props.nest;
-//     //     const user = Object.assign({}, this.state);
-//     //     nest(user);
-//     //     this.setState({ processed: true });
-
-//     // }
-
-//     handleSubmit(modal) {
-//         return (e) => {
-//             e.preventDefault();
-//             if (this.state.processed) {
-//                 this.setState({ processed: false }, () => this.props.closeModal());
-//             } else {
-//                 this.setState({ processed: true }, () => this.props.openModal(modal));
-//             }
-//         }
-//     }
-
-//     render() {
-//         return (
-//             <div className="navbar">
-//                 <div className="nav-left">
-//                     <div className="dropdown">
-//                         <span className="nav-link" to="/shoes/women">WOMEN</span>     
-//                         <i className="fas fa-caret-down icon"></i>
-//                         {/* <button className="dropbtn">FEMALE */}
-//                         {/* <i className="fa fa-caret-down"></i>
-//                         </button> */}
-//                         <div className="dropdown-content">
-//                             <Link to="/shoes/women">SHOP ALL BIRDS</Link>
-//                             {/* <a href="#">Shop ALL BIRDS</a> */}
-//                         </div>
-//                     </div>
-
-//                     <div className="dropdown">
-//                         <span className="nav-link" to="/shoes/men">MEN</span>
-//                         <i className="fas fa-caret-down icon"></i>
-//                         {/* <button className="dropbtn">MALE
-//                         <i className="fa fa-caret-down"></i>
-//                         </button> */}
-//                         <div className="dropdown-content">
-//                             <Link to="/shoes/men">SHOP ALL BIRDS</Link>
-//                         </div>
-//                     </div>
-
-//                 </div>  
-//                     <style>
-//                         @import url('https://fonts.googleapis.com/css?family=Lobster+Two:400i&display=swap');
-//                     </style>
-                    
-
-//                     <Link className="logo" to='/'>birds</Link>
-
-//                 <div className="nav-right">
-//                     <a href="#materials">MATERIALS</a>
-//                     <a href="#branches">BRANCHES</a>
-//                     <a href="#account"><i className="far fa-user icon"></i></a>
-//                     <a href="?"><i className="far fa-question-circle icon"></i></a>
-//                     {/* <button onClick={this.handleSubmit("shoe index")}><i className="fas fa-shopping-cart icon"></i></button> */}
-//                     <button className="icon-button cart-button" onClick={this.handleSubmit("shoe index")}><i className="fas fa-shopping-cart" /></button>
-//                 </div>
-//             </div>
-//         );
-//     }
-// }
-
-// class NavbarForm extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             processed: false, 
-//             showDropdown: false
-//         };
-//         this.handleSubmit = this.handleSubmit.bind(this);
-//         this.showDropdown = this.showDropdown.bind(this);
-//         this.closeDropdown = this.closeDropdown.bind(this);
-//     }
-
-//     showDropdown(e) {
-//         e.preventDefault();
-        
-//         this.setState({ showDropdown: true }, () => {
-//             document.addEventListener('click', this.closeDropdown);
-//         });
-//     }
-
-//     closeDropdown() {
-//         this.setState({ showDropdown: false }, () => {
-//             document.removeEventListener('click', this.closeDropdown);
-//         });
-//     }
-
-//     handleSubmit(modal) {
-//         return (e) => {
-//             e.preventDefault();
-//             if (this.state.processed) {
-//                 this.setState({ processed: false }, () => this.props.closeModal());
-//             } else {
-//                 this.setState({ processed: true }, () => this.props.openModal(modal));
-//             }
-//         }
-//     }
-
-//     render() {
-//         return (
-//             <div className="navbar">
-//                 <div className="nav-left">
-
-//                 </div>
-
-//                 <style>
-//                     @import url('https://fonts.googleapis.com/css?family=Lobster+Two:400i&display=swap');
-//                 </style>
-//                 <Link className="logo" to='/'>birds</Link>
-
-//                 <div className="nav-right">
-//                     <div className="nav-right-links">
-//                         <a href="#materials">MATERIALS</a>
-//                         <a href="#branches">BRANCHES</a>
-//                     </div>
-//                     <a href="#account"><i className="far fa-user icon"></i></a>
-//                     <a href="?"><i className="far fa-question-circle icon"></i></a>
-//                     <button className="icon-button cart-button" onClick={this.handleSubmit("shoe index")}><i className="fas fa-shopping-cart" /></button>
-//                 </div>
-//             </div>
-//         );
-//     }
-// }
-
-// export default NavbarForm;
-
-
 import React from 'react';
 
 import { Link } from 'react-router-dom';
 
-import ShoesDropdown from './nav_dropdown';
-
-import { withRouter } from 'react-router-dom';
-
-
-
 class NavbarForm extends React.Component {
     constructor(props) {
         super(props);
+
+        this.handleSubmit = this.handleSubmit.bind(this);
         this.state = {
-            processed: false, 
-            // showDropdown: false,  
-            // closeDropdown: false,       
-        };
-        this.handleSubmit = this.handleSubmit.bind(this);    
-        // this.showDropdown = this.showDropdown.bind(this);  
-        // this.closeDropdown = this.closeDropdown.bind(this); 
+            processed: false
+        }
     }
-    // showDropdown(e) {
-    //     e.preventDefault();
-
-    //     this.setState({ showDropdown: true }, () => {
-    //         document.addEventListener('click', this.closeDropdown);
-    //     });
-    // }
-
-    // closeDropdown() {
-    //     this.setState({ showDropdown: false }, () => {
-    //         document.removeEventListener('click', this.closeDropdown);
-    //     });
-    // }
 
     handleSubmit(modal) {
         return (e) => {
             e.preventDefault();
+            
             if (this.state.processed) {
                 this.setState({ processed: false }, () => this.props.closeModal());
             } else {
@@ -193,40 +25,26 @@ class NavbarForm extends React.Component {
     }
 
     render() {
-       
         return (
             <div className="navbar">
                 <div className="nav-left">
                     <div className="dropdown-women">
-                        <span className="nav-link" to="/shoes/women">WOMEN</span> 
-                        <i className="fas fa-angle-down"></i>  
-                        {/* <i onclick="myFunction(this)" className="fas fa-angle-down"></i>
-                        <script>
-                            function myFunction(x) {
-                                x.classList.toggle("&minus;")
-                            }
-                        </script>      */}
-                        <div className="dropdown-content">
-                            <Link to="/shoes/women">SHOP ALL BIRDS</Link>
-                             {/* <a href="#">Shop ALL BIRDS</a> */}
-                         </div>
+                        <span className="nav-link" to="/shoes/women">WOMEN</span>
+                        <i className="fas fa-angle-down"></i>
                     </div>
                     <div className="dropdown-men">
-                        <span className="nav-link" to="/shoes/men">MEN</span>
+                        <span className="nav-link" to="/shoes/women">MEN</span>
                         <i className="fas fa-angle-down"></i>
-                        <div className="dropdown-content">
-                            <Link to="/shoes/men">SHOP ALL BIRDS</Link>
-                            {/* <a href="#">Shop ALL BIRDS</a> */}
-                        </div>
                     </div>
                 </div>
 
-                <style>
-                    @import url('https://fonts.googleapis.com/css?family=Lobster+Two:400i&display=swap');
-                </style>
+                <div className="logo">
+                    <style>
+                        @import url('https://fonts.googleapis.com/css?family=Lobster+Two:400i&display=swap');
+                    </style>
+                    <Link className="logo" to='/'>birds</Link>
 
-
-                <Link className="logo" to='/'>birds</Link>
+                </div>
 
                 <div className="nav-right">
                     <div className="nav-materials">
@@ -236,14 +54,12 @@ class NavbarForm extends React.Component {
                         <a href="#branches">BRANCHES</a>
                     </div>
 
-                    <div className="nav-icons">
+                    {/* <div className="nav-icons">
                         <a href="#account"><i className="far fa-user icon"></i></a>
                         <a href="?"><i className="far fa-question-circle icon"></i></a>
                         <button className="icon-button cart-button" onClick={this.handleSubmit("shoe index")}><i className="fas fa-shopping-cart" /></button>
-                    </div>
-
+                    </div> */}
                 </div>
-                
             </div>
         );
     }
