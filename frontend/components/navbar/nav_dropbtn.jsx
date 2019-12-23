c3787e59ef0f2a5a45d8a33aa3f5cec66ee09913
zhenjess <jessicazhen8@gmail.com>
2019-12-23 10:06:50 -0800

    Animate nav gender dropdowns

--- frontend/components/navbar/nav_dropbtn.jsx
+++ frontend/components/navbar/nav_dropbtn.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 
-const dropDownBtn = ({ activeHeader, showDropdown, dropDown, title, id, dropDownGender }) => {
-    const dropOrNot = ({ dropDown, gender, componentGender }) => {
+const dropDownBtn = ({ activeHeader, showDropDown, dropDown, title, id, dropDownGender }) => {
+
+    const DropOrNot = ({ dropDown, gender, componentGender }) => {
         if (dropDown && gender === componentGender) {
-            return (<h5 className="minus">&minus;</h5>);
+            return (<h5 className="minus"> &minus;</h5>);
         } else {
-            return (<h5 className="downCaret"><i className="fas fa-angle-down"></i></h5>);
+            return (<h5 className="drop-down"> <i className="fas fa-angle-down"></i></h5>);
         }
     };
 
     return (
         <>
-        <div className={(activeHeader ? 'a-header-button' : 'u-header-button') + ' nav-link'}
-                onClick={() => showDropdown(id)}>
+            <div className={(activeHeader ? "a-header-button" : "u-header-button") + " nav-link"}
+                onClick={() => showDropDown(id)}>
                 <h2>{title}</h2>
-                <dropOrNot dropDown={dropDown} gender={dropDownGender} componentGender={id}/>
-        </div>
+                <DropOrNot dropDown={dropDown} gender={dropDownGender} componentGender={id} />
+            </div>
         </>
     )
 };
--- frontend/components/navbar/navbar_form.jsx
+++ frontend/components/navbar/navbar_form.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 
 import DropdownBtn from './nav_dropbtn';
+import ShoesDropdown from './nav_dropdown';
 
 
 class NavbarForm extends React.Component {
@@ -70,46 +71,73 @@ class NavbarForm extends React.Component {
         const dropDown = this.state.dropDown;
 
         return (
-            <div className={(dropDown && activeHeader) ? 'active-header-with-dropdown' :
-             (activeHeader) ? 'active-header' : 'header'} id='header'>
-
-            </div>
-            <div className="navbar">
-                <div className="nav-left">
-                    <div className="dropdown-women">
-                        <span className="nav-link" to="/shoes/women">WOMEN</span>
-                        <i className="fas fa-angle-down"></i>
+            <>
+            <div className={(dropDown && activeHeader) ? 'active-header-with-dropdown' : (activeHeader) ? 'active-header' : 'header'} id='header'>
+                <div id="navs">
+                    <div className="navbar" id="nav-left">
+                        <dropDownBtn
+                            activeHeader={activeHeader}
+                            showDropdown={this.showDropdown}
+                            dropDown={this.state.dropDown}
+                            dropDownGender={this.state.dropDownGender}
+                            title="WOMEN"
+                            id="WOMEN"
+                        />
+                        <dropDownBtn
+                            activeHeader={activeHeader}
+                            showDropdown={this.showDropdown}
+                            dropDown={this.state.dropDown}
+                            dropDownGender={this.state.dropDownGender}
+                            title="MEN"
+                            id="MEN"
+                        />
                     </div>
-                    <div className="dropdown-men">
-                        <span className="nav-link" to="/shoes/women">MEN</span>
-                        <i className="fas fa-angle-down"></i>
-                    </div>
-                </div>
-
-                <div className="logo">
-                    <style>
-                        @import url('https://fonts.googleapis.com/css?family=Lobster+Two:400i&display=swap');
-                    </style>
-                    <Link className="logo" to='/'>birds</Link>
-
-                </div>
-
-                <div className="nav-icons">
-                    <div className="nav-right">
-                        <div className="nav-materials">
-                            <a href="#materials">MATERIALS</a>
-                        </div>
-                        <div className="nav-branches">
-                            <a href="#branches">BRANCHES</a>
-                        </div>
-                    </div>
-                    <a href="#account" className="icon-button cart-button" ><i className="far fa-user icon"></i></a>
-                    <a href="?" className="icon-button cart-button" ><i className="far fa-question-circle icon"></i></a>
-                    <button className="icon-button cart-button" onClick={this.handleSubmit("shoe index")}><i className="fas fa-shopping-cart" /></button>
                 </div>
             </div>
+
+            <div 
+                onAnimationEnd={this.handleAnimation} 
+                id="nav-dropdown" className={!this.state.dropDown ? 'invisible' : !this.state.isAnimating ? 'visible' : this.state.bounce ? 'visible swipe-up-down' : 'visible swipe-down'}>
+                <ShoesDropdown gender={this.state.dropDownGender} genderQuery={this.state.dropDownGender === 'WOMEN' ? 'womens' : 'mens'} showDropdown={this.showDropdown} />
+            </div>
+            <div className={dropDown ? 'overlay-visible' : 'overly-invisible'}></div>
+            </>
+            // <div className="navbar">
+            //     <div className="nav-left">
+            //         <div className="dropdown-women">
+            //             <span className="nav-link" to="/shoes/women">WOMEN</span>
+            //             <i className="fas fa-angle-down"></i>
+            //         </div>
+            //         <div className="dropdown-men">
+            //             <span className="nav-link" to="/shoes/women">MEN</span>
+            //             <i className="fas fa-angle-down"></i>
+            //         </div>
+            //     </div>
+
+            //     <div className="logo">
+            //         <style>
+            //             @import url('https://fonts.googleapis.com/css?family=Lobster+Two:400i&display=swap');
+            //         </style>
+            //         <Link className="logo" to='/'>birds</Link>
+
+            //     </div>
+
+            //     <div className="nav-icons">
+            //         <div className="nav-right">
+            //             <div className="nav-materials">
+            //                 <a href="#materials">MATERIALS</a>
+            //             </div>
+            //             <div className="nav-branches">
+            //                 <a href="#branches">BRANCHES</a>
+            //             </div>
+            //         </div>
+            //         <a href="#account" className="icon-button cart-button" ><i className="far fa-user icon"></i></a>
+            //         <a href="?" className="icon-button cart-button" ><i className="far fa-question-circle icon"></i></a>
+            //         <button className="icon-button cart-button" onClick={this.handleSubmit("shoe index")}><i className="fas fa-shopping-cart" /></button>
+            //     </div>
+            // </div>
         );
     }
 }
 
-export default withRouter(NavbarForm);
+export default NavbarForm;
