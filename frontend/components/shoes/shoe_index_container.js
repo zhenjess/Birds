62cbd58c3b15804b95da5b95f850521a83c5b925
zhenjess <jessicazhen8@gmail.com>
2019-12-30 09:52:25 -0800

    Select items from shoe index to add to cart

--- frontend/components/shoes/shoe_index_container.js
+++ frontend/components/shoes/shoe_index_container.js
@@ -4,11 +4,11 @@ import ShoeIndex from './shoe_index';
 
 import { fetchAllShoes, fetchShoe } from '../../actions/shoes_actions';
 
-import { selectAllShoes, selectAllShoesByGender } from '../../reducers/selectors';
+import { selectAllShoes, selectAllShoesByGender, selectItemsByGender } from '../../reducers/selectors';
 
 import{ addToCart, removeFromCart } from '../../actions/cart_actions';
 
-import { fetchItems } from '../../actions/items_actions';
+import { fetchShoeItems } from '../../actions/items_actions';
 
 const mapStateToProps = (state, ownProps) => ({
     //shoes: Object.values(state.entities.shoes),
@@ -16,16 +16,14 @@ const mapStateToProps = (state, ownProps) => ({
     //selector to return an array of shoes with name material and shoe id
     shoes: selectAllShoesByGender(state, ownProps.match.params.gender), //to hard code by gender
 
-    return ({
-        items: items
-    });
+    items: selectItemsByGender(state, ownProps.match.params.id)
 
 });
 
 const mapDispatchToProps = dispatch => ({
     fetchAllShoes: () => dispatch(fetchAllShoes()),
     fetchShoe: (shoeId) => dispatch(fetchShoe(shoeId)),
-    fetchItems: (id) => dispatch(fetchItems(id)),
+    fetchShoeItems: (id) => dispatch(fetchShoeItems(id)),
     addToCart: (item, size) => dispatch(addToCart(item, size)),
     removeFromCart: (id, size) => dispatch(removeFromCart(id, size))
 });
