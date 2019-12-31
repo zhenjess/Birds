f831698c9675548e3b1d290a2f498461d452dac6
zhenjess <jessicazhen8@gmail.com>
2019-12-30 22:28:34 -0800

    Join multiple tables to reference gender column in shoes table

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,16 @@
 class Item < ApplicationRecord
+    validates :size, :price, :shoe_option_id, presence: true
+
     has_many :cart_items
     belongs_to :shoe_option
     has_one_attached :photo #do active storage here
 
-    # def self.find_by_gender(search_id)
-    #     Item.joins(:show_option)
-    #         .where(shoes_option: { gender: search_id })
-    #         .select("items.id", :shoe_option_id, :color, :model, :material, :gender, :price)
-    # end
-    def self.find_by_gender(search_id) 
-        Item.joins(:shoe_option)
-            .where(shoe_options: {gender: search_id})
-            .select('items.id', :color, :model, :material, :gender, :shoe_option_id, :shoe_id, :color_id, :material_id)
+    # ShoeOption.joins(:items, :shoe)
+    
+    def self.find_by_gender(search_id)
+        Item.joins(:shoe_option, :shoe) 
+        .where("item.shoe_option_id = shoe_option.id", "shoe_option.shoe_id = shoe.id", shoes: {gender: search_id})
+        .select("items.id", :size, :price, :shoe_id, :model, :gender, :shoe_option_id, :color_id, :material_id)
     end
+    
 end
