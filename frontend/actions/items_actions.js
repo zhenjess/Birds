08254b0df17f30fa7e350870c2827580a32bd97e
zhenjess <jessicazhen8@gmail.com>
2019-12-31 11:46:26 -0800

    Update action items

--- frontend/actions/items_actions.js
+++ frontend/actions/items_actions.js
@@ -1,20 +1,6 @@
 import * as APIUtil from '../util/items_api_util';
 
-// import { receiveShoeItems } from '../util/items_api_util';
-
 export const RECEIVE_ITEMS = 'RECEIVE_ITEMS';
-
-// const fetchShoeItems = (items) => {
-//     return ({
-//         type: RECEIVE_ITEMS, 
-//         items: items
-//     });
-// };
-
-// export const fetchItems = id => dispatch => (
-//     receiveShoeItems(id)
-//         .then( items => dispatch(fetchShoeItems(items)))
-// );
 
 const receiveItems = items => ({
     type: RECEIVE_ITEMS, 
