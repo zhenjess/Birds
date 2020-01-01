1e63ce7681c68b9e5542b9318e9b6307a4493a1d
zhenjess <jessicazhen8@gmail.com>
2019-12-31 21:25:48 -0800

    Create shoe filter bar

---
+++ frontend/components/shoes/filter.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class Filter extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const filters = this.props.filters.map( filter => (
+            <li className={`${this.props.id}-dropdown-attributes`}
+                onClick={() => this.props.addFilter(this.props.id, filter)}
+                id={filter}
+                key={filter}
+            ><h4>{filter}</h4>
+            </li>
+        ));
+        return (
+            <div className="shoe-bar-filters">
+                <ul className="shoe-bar-filter-container">{filters}</ul>
+            </div>
+        );
+    }
+}
+
+export default Filter;
