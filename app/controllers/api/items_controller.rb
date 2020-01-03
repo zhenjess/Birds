7ae4dfe6c56d8ff660afdf58bba5f30473d8a09b
zhenjess <jessicazhen8@gmail.com>
2020-01-03 11:06:23 -0800

    Update shoe model

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,13 +1,13 @@
 class Api::ItemsController < ApplicationController
-    def new 
-        @items = Item.new
-        render :new
-    end
-    def index
-        @items = Item.all
-        @shoe_options = ShoeOption.all
-        @shoes = Shoe.all
-    end
+    # def new 
+    #     @items = Item.new
+    #     render :new
+    # end
+    # def index
+    #     @items = Item.all
+    #     @shoe_options = ShoeOption.all
+    #     @shoes = Shoe.all
+    # end
 
     def show
         @items = Item.find_by_gender(params[:id])
